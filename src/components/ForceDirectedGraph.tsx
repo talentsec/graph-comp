05ffd81840f4bc3603d744ecfbf95865fc75da04
REND42 <rend42@163.com>
2022-06-23 11:15:05 +0800

    init

---
+++ src/components/ForceDirectedGraph.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import data from '../mock/test.json'
+
+const ForceDirectedGraph = () => {
+  
+  const data = useState({})
+
+  console.log('test', data)
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default ForceDirectedGraph
