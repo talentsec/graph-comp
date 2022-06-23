6646c06e10c81f2d1cb865011e2f60f2012c28ae
REND42 <rend42@163.com>
2022-06-23 17:50:29 +0800

    update

--- src/components/ForceDirectedGraph.tsx
+++ src/components/ForceDirectedGraph.tsx
@@ -1,15 +1,42 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import data from '../mock/test.json'
+import './ForceDirectedGraph.css'
+import { VizSimulation } from '../utils/createGraph'
+
+interface IPorps {
+  type: 'tree' | 'graph'
+}
+
+type VizType = 'graph' | 'tree'
 
 const ForceDirectedGraph = () => {
-  
-  const data = useState({})
 
-  console.log('test', data)
+  const [vizType, setVizType] = useState<VizType>('tree')
+
+  const simulation = useRef<VizSimulation | null>(null)
+
+  const switchVizType = () => {
+    if(vizType === 'graph') {
+      setVizType('tree')
+    } else {
+      setVizType('graph')
+    }
+  }
+
+  useEffect(() => {
+    if(!simulation.current) {
+      simulation.current = new VizSimulation(data, vizType)
+    }
+    if(simulation.current.status === 'created') {
+      simulation.current.restart(vizType)
+    } else {
+      simulation.current.create()
+    }
+  }, [vizType])
 
   return (
-    <div>
-
+    <div id='graph'>
+      <button id='btn' onClick={() => { switchVizType() }}>switch</button>
     </div>
   )
 }
